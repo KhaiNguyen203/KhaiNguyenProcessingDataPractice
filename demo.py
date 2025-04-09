9f3d82bba4d66075419f88b1e5e4bc8a7770507e
unknown <diepkhainguyen9117@gmail.com>
2025-04-09 11:22:05 +0700

    practiceGroupby

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('Spotify_2024_Global_Streaming_Data.csv')
-print(df.sort_values("Total Streams (Millions)").head(10), df[['Artist', 'Total Streams (Millions)']])
+print( df.groupby(['Artist', 'Monthly Listeners (Millions)'])['Total Streams (Millions)'].sum())
