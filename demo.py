24f9e960bd2722d399f1af02033d655b79b9c7a5
unknown <diepkhainguyen9117@gmail.com>
2025-04-09 08:52:52 +0700

    UniqueArtistInDataSet

--- demo.py
+++ demo.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('Spotify_2024_Global_Streaming_Data.csv')
-pd.set_option('display.max_columns', None)
-print(df[df['Total Hours Streamed (Millions)'] == df['Total Hours Streamed (Millions)'].max()])
+print (df['Artist'].unique())
 
