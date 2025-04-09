38d84c4e4f4266853385662ff5d0b01eece7ad1d
unknown <diepkhainguyen9117@gmail.com>
2025-04-09 16:34:33 +0700

    2014-2025_April_WeatherForecastDataInBasel-DataProcessing1

--- demo.py
+++ demo.py
@@ -1,4 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv('Spotify_2024_Global_Streaming_Data.csv')
-print( df.groupby(['Artist', 'Monthly Listeners (Millions)'])['Total Streams (Millions)'].sum())
+
+BaTemp = 'Basel Temperature'
+BaPrec = 'Basel Precipitation Total'
+
+df = pd.read_csv('2014-2025_April_WeatherForecastDataInBasel.csv')
+# chuyển dạng timestamp sang dạng yyyy-mm-dd
+df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y%m%dT%H%M')
+#Gộp phần giờ của mẫu số liệu chỉ lấy theo ngày
+df['day'] = df['timestamp'].dt.to_period('D')
+#tính trung bình dữ liệu của các giờ trong ngày
+df_day = df.groupby('day').mean()
+#dropna để loại bỏ các ô dữ liệu bị trống
+print(df_day[['Basel Temperature', 'Basel Precipitation Total']].dropna())
