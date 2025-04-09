b1b8e7ba62efe1c059e3f5b53bf2ac29685a3898
unknown <diepkhainguyen9117@gmail.com>
2025-04-09 09:24:41 +0700

    10MostTotalStreamsArtist-2ColumnsArTist

--- demo.py
+++ demo.py
@@ -1,5 +1,4 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('Spotify_2024_Global_Streaming_Data.csv')
-print (df['Artist'].unique())
-
+print(df.sort_values("Total Streams (Millions)").head(10), df[['Artist', 'Total Streams (Millions)']])
