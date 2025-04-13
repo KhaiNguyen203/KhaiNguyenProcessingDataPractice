9debb8bcd4e9b5d11d88609dc9126c9d50d7cd3e
unknown <diepkhainguyen9117@gmail.com>
2025-04-13 14:56:44 +0700

    v6

--- pw1.py
+++ pw1.py
@@ -35,21 +35,6 @@ df_day = df_day[['Basel Temperature', 'Basel Precipitation Total']].dropna()
 df_train = df_day[:'2024-12-31']
 df_test = df_day['2025-01-01':]
 
-##vẽ biểu đổ 
-# Chuyển 'date' từ index thành cột
-df_day = df_day.reset_index()
-ax = df_day.plot(kind='scatter', x="date", y='Basel Temperature')
-
-# điều chỉnh độ chia nhỏ nhất của trục y và trục x
-ax.xaxis.set_major_locator(mdates.YearLocator(1))
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
-ax.yaxis.set_major_locator(ticker.MultipleLocator(1))  # mỗi 1 độ
-
-plt.title('Temperature & Precipitation of Basel in April (2014 - 2025)')
-
-#*plt.show()
-#*print (df_train[['date', 'Basel Temperature', 'Basel Precipitation Total']])
-
 #rút trích dữ liệu từ tháng 4 của các năm 2014-2023
 df_train = df_train.reset_index()
 df_april = df_train[df_train['date'].dt.month == 4]
@@ -74,12 +59,37 @@ training_data = np.array([np.array(temp) for temp in temp_2014to2023])  # shape 
 # Chuyển temp_2015to2024 thành mảng 1D (số ngày trong tháng 4 của năm 2024)
 lable_data = np.array(temp_2015to2024)
 
+'''
 print("Training data shape:", training_data.shape)
 print("Label data shape:", lable_data.shape)
 print(lable_data)
+'''
 
 #tạo mô hình dự đoán
-predictionModel = LinearRegression()
-predictionModel.fit(training_data,lable_data)
+PModel = LinearRegression()
+PModel.fit(training_data,lable_data)
+#! Phương thức values sẽ chuyển từ Series của pandas về array của numpy giúp chúng hoạt động được trong phương thức predict của scikitlearn
+Temp_April_2024 = df_april[df_april['date'].dt.year == 2024]['Basel Temperature'].values
 
-#*print (lable_data)
+#! trong cấu trúc của reshape giúp biến mảng 1D (hiện tại là Temp_April_2024) biến thành mảng 2D (input_2024) 
+##! số 1 biểu diễn cho số hàng của mảng 2D (chỉ có 1 hàng vì chỉ xét trong tháng 4 của năm 2024)
+##! số 30 biêu diễn cho số cột của mảng 2D (30 ngày)
+#phương thức predict yêu cầu 1 mảng 2D vì vậy cần chuyển từ 1D sang 2D
+Temp_April_2024_reshape = Temp_April_2024.reshape(1, 30)
+
+predicted_2025 = PModel.predict(Temp_April_2024_reshape)
+print(predicted_2025)
+
+#vẽ biểu đổ 
+## Chuyển 'date' từ index thành cột
+df_day = df_day.reset_index()
+ax = df_day.plot(kind='scatter', x="date", y='Basel Temperature')
+
+## điều chỉnh độ chia nhỏ nhất của trục y và trục x
+ax.xaxis.set_major_locator(mdates.YearLocator(1))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))  # mỗi 1 độ
+
+plt.title('Temperature & Precipitation of Basel in April (2014 - 2025)')
+
+#*plt.show()
