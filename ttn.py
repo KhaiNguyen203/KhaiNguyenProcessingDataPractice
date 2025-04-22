162ac62fd9511a9ec133b56ccddd2d969718bb48
unknown <diepkhainguyen9117@gmail.com>
2025-04-22 16:04:09 +0700

    ver3

--- ttn.py
+++ ttn.py
@@ -4,33 +4,60 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
  
 df = pd.read_csv("train.csv")
+
 '''
 df.plot(kind = 'scatter', x='PassengerId', y="Survived")
 plt.show()
 '''
+
 df_label = df['Survived']
-df_train = df['Sex']
-
+df_train = df[['Pclass', 'Sex']]
 list_train = []
-change_to_list_2D = []
 for i in range (0, 891):
-    tmp = df['Sex'].loc[i]
-    if(tmp == 'male'): list_train.append(1)
-    else: list_train.append(0)
-
+    tmp = []
+    #item giúp chuyển từ kiểu dữ liệu của numpy về python
+    tmp.append(df_train['Pclass'].loc[i].item())
+    if(df_train['Sex'].loc[i] == 'male'):
+        tmp.append(1)
+    else:
+        tmp.append(0)
+    list_train.append(tmp)
 list_label =[]
 for i in range(0, 891):
     list_label.append(df['Survived'].loc[i])
-df_train = np.array(list_train)
-df_train = df_train.reshape(1, 891)
-df_label = np.array(list_label)
-df_label = df_label.reshape(1, 891)
-#*print(df_train)
-#*print (df_train.shape)
-#*print (df_label.shape)
+
+ar_train = np.array(list_train)
+ar_label = np.array(list_label)
+
+df_test = pd.read_csv("test.csv")
+df_test = df_test[['PassengerId', 'Pclass', 'Sex']]
+list_test = []
+for i in range (1, 418):
+    tmp = []
+    #item giúp chuyển từ kiểu dữ liệu của numpy về python
+    tmp.append(df_test['Pclass'].loc[i].item())
+    if(df_test['Sex'].loc[i] == 'male'):
+        tmp.append(1)
+    else:
+        tmp.append(0)
+    list_test.append(tmp)
+ar_test = np.array(list_test)
 
 model = LinearRegression()
-model.fit(df_train, df_label)
+model.fit(ar_train, ar_label)
+
+survived_prediction = model.predict(ar_test)
+survived_prediction = survived_prediction.flatten()
+#transfer PassengerId col from df_test to array 1D
+transfer_col_PassengerId = []
+for i in range(1, 418):
+    transfer_col_PassengerId.append(df_test['PassengerId'].loc[i].item())
+    if(survived_prediction[i-1] >= 0.5): survived_prediction[i-1] = int(1)
+    else: survived_prediction[i-1] = int(0)
+df_result = pd.DataFrame({"PassengerId": transfer_col_PassengerId, "Survived": survived_prediction})
+print (df_result)
+#* df_result.to_csv('result.csv', index=False)
+
 ###
 ###
 ###
